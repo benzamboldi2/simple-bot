6a0218d92556c3126d537063e08ee631f8d4da8d
Ben <rbzamboldi@email.wm.edu>
2022-09-20 02:09:40 -0400

    Scraper scrapes Jan, Feb, Mar.

--- scraper/web_scraper.py
+++ scraper/web_scraper.py
@@ -3,17 +3,17 @@ from bs4 import BeautifulSoup
 import json
 import string
 
-class National_Days:
+class NationalDays:
 
     def __init__(self):
         self.days = {}
-        self.baseURL = "https://nationaldaycalendar.com/"
+        self.base = "https://nationaldaycalendar.com/"
 
     def get_month(self, month):
         if month not in self.days:
             self.days[month] = {}
 
-        URL = self.baseURL + month + "/"
+        URL = self.base + month + "/"
 
         page = requests.get(URL)
 
@@ -29,55 +29,39 @@ class National_Days:
             temp = word.strip()
             last = len(temp) - 1
             check_indicator = temp[last - 6] + temp[last - 5] + temp[last - 4] + temp[last - 3] + temp[last - 2] + temp[last - 1]+ temp[last]
-            if check_indicator == "Day</a>":
-                day, first = self.get_day(temp, last)
-                if day == "Register A Day":
+            double_check = temp[last - 12] + temp[last - 11] + temp[last - 10] + temp[last - 9] + temp[last - 8] + temp[last - 7] + temp[last - 6] + temp[last - 5] + temp[last - 4]
+            if check_indicator == "Day</a>" and double_check != "ter A Day":
+                day, date = self.get_day(temp)
+                if day == False:
                     continue
                 else:
-                    date = self.get_date(temp, first)
                     if date not in self.days[month]:
                         self.days[month][date] = []
                     self.days[month][date].append(day)
 
-    def get_day(self, string, last):
-        ptr = last - 6
+    def get_day(self, string):
+        ptr = 41
         tmp = ""
-        while tmp != ">":
-            tmp = string[ptr - 1]
-            ptr = ptr - 1
+        while string[ptr] !="/":
+            tmp = tmp + string[ptr]
+            ptr = ptr + 1
 
-        day = ""
-        for i in range(ptr + 1, last - 3):
-            if string[i] == "\u2019":
-                day = day + "'"
-            else:
-                day = day + string[i]
+        words = tmp.split('-') 
+
+        if words[len(words) - 1] == "day" or words[0] != "national":
+            return (False, False)
+
+        ptr = 0
+        day = words[ptr]
+        while words[ptr] != "day":
+            ptr = ptr + 1
+            day = day + " " + words[ptr]
+
+        date = words[ptr + 1]
+        for i in range(ptr + 2, len(words)):
+            date = date + " " + words[i]
         
-        return (day, ptr)
-
-
-    def get_date(self, string, first):
-        print(string)
-        print(string[first - 3] + string[first - 2] + string[first - 1] + string[first])
-        ptr = first
-        tmp = string[ptr]
-        while not tmp.isnumeric():
-            print(tmp)
-            tmp = string[ptr - 1]
-            ptr = ptr - 1
-
-        date = tmp
-
-        if string[ptr].isnumeric():
-            date = string[ptr] + tmp
-        # while tmp != "\"":
-        #     date = date + tmp
-        #     tmp = string[ptr + 1]
-        #     ptr = ptr + 1
-            # day = day + string[i]
-
-        
-        return date
+        return(day, date)
 
     def main(self, months):
         for month in months:
@@ -87,80 +71,7 @@ class National_Days:
         with open('data.json', 'w') as fp:
             json.dump(self.days, fp, indent=4)
         
-
-
 if __name__ == "__main__":
-    scraper = National_Days()
-    months = ["January"]
+    scraper = NationalDays()
+    months = ["January", "February", "March"]
     scraper.main(months)
-
-# # URL = "https://nationaldaycalendar.com/january/"
-# # page = requests.get(URL)
-
-# # soup = BeautifulSoup(page.content, "html.parser")
-
-# # results = soup.find_all('a')
-
-# # print(len(results))
-
-# days = []
-
-# for item in results:
-#     word = str(item)
-#     temp = word.strip()
-#     last = len(temp) - 1
-#     check_indicator = temp[last - 6] + temp[last - 5] + temp[last - 4] + temp[last - 3] + temp[last - 2] + temp[last - 1]+ temp[last]
-#     if check_indicator == "Day</a>":
-#         print(word)
-
-#         ptr = last - 6
-#         tmp = ""
-#         while tmp != ">":
-#             tmp = temp[ptr - 1]
-#             ptr = ptr - 1
-
-#         day = ""
-#         for i in range(ptr + 1, last - 3):
-#             day = day + temp[i]
-        
-#         days.append(day)
-        
-
-#     # else:
-#     #     results.remove(item)
-
-# print(days)
-#     # if last > 7:
-#     #     print(temp[last - 6] + temp[last - 5] + temp[last - 4] + temp[last - 3] + temp[last - 2] + temp[last - 1]+ temp[last])
-#     # print("\n\n\n")
-
-# # print(len(list(soup.children)))
-
-# # results = soup.a
-
-# # print(results)
-
-# # results = soup.find(id="ff-main-container")
-
-# # print(results.prettify())
-
-# # for item in results:
-#     # print(item)
-
-# # main_content = results.find_all("div", class_="ndc-text-january-1---10-national-days")
-
-# # for item in main_content:
-# #     print(item)
-
-# # job_elements = main_content.find_all("div", class_="ndc-text-january-1---10-national-days")
-
-# print("\n\n BREAK \n\n")
-
-# # print(main_content.a)
-
-# # for ele in main_content:
-# #     # day = ele.find("a", class_="day")
-# #     # print(day)
-# #     print(ele, end="\n"*2)
-
-# # test = main_content.find("li", class_="title")
